ab6e55300b32bced72d58e8027216102854acb90
DaviFreitas87 <49248338+DaviFreitas87@users.noreply.github.com>
2022-08-19 10:14:41 -0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/cadastrar-livro', function(Request $request) {
+
+    \App\Models\Biblioteca::create([
+    'titulo' => $request->titulo,
+    'isbn' => $request-> isbn,
+    'nome_autor' => $request-> nome_autor,
+    'ano' =>  $request-> ano
+
+    ]);
+
+    echo "Livro criado com sucesso!";
+
+});
+
+Route::get('/ver-livro/{id}', function ($id){
+$biblioteca = \App\Models\Biblioteca::find($id);
+return view ('ver', ['biblioteca' => $biblioteca]);
+});
+
+Route::get('/editar-livro/{id}', function(Request $request, $id) {
+
+    $biblioteca = \App\Models\Biblioteca::find($id);
+
+    return view ('editar', ['biblioteca' => $biblioteca]);
+
+    $biblioteca::update([
+        'titulo' => $request->titulo,
+        'isbn' => $request-> isbn,
+        'nome_autor' => $request-> nome_autor,
+        'ano' =>  $request-> ano
+    
+        ]);
+
+        echo "Livro EDITADO com sucesso!";
+
+});
+
---
+++ storage/framework/views/296106906283ef0e16b702ebb94358f32b76d4ec.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TESTE PRATICO </title>
+</head>
+<body>
+   
+<div id="cabeçalho" style="text-align: center; color:white; background-color:red">
+    <br>
+
+    BIBLIOTECA
+    
+<br><br>
+</div> 
+
+<br><br>
+
+<form action="/editar-livro/<?php echo e($biblioteca-> id); ?>" method="GET">
+<?php echo csrf_field(); ?>
+<label>Titulo do livro: </label>
+<input type="text" name="titulo" value="<?php echo e($biblioteca->titulo); ?>">
+<br><br>
+<label>ISBN: </label>
+<input type="text" name="isbn" value="<?php echo e($biblioteca->isbn); ?>">
+<br><br>
+<label>Nome do Autor: </label>
+<input type="text" name="nome_autor" value="<?php echo e($biblioteca->nome_autor); ?>">
+<br><br>
+<label>Ano do lançamento: </label>
+<input type="text" name="ano" value="<?php echo e($biblioteca->ano); ?>">
+<br><br>
+
+
+
+<button> ENVIAR NOVO LIVRO </button>
+
+
+</form>
+
+
+
+
+</body>
+</html><?php /**PATH C:\Users\tecnologia02\Desktop\biblioteca\resources\views/editar.blade.php ENDPATH**/ ?>
